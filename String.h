b27f7a3bfc326ca121be2c7603ac12abfe120226
Krypton-46 <musabismail02@gmail.com>
2024-05-16 17:50:19 +0500

    Create String.h
    Header File

---
+++ String.h
@@ -0,0 +1,15 @@
+#ifndef STRING_H
+#define STRING_H
+#include<iostream>
+using namespace std;
+enum Sign{POSITIVE,NEGATIVE};
+class String
+{
+	char* data;
+	int size;
+public:
+	String();
+	String(const char);
+	String(const char*);
+	~String();
+	bool isEmpty()const;
