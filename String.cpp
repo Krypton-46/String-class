05749867eb4c69059a7076c88ff3a9341054b619
Krypton-46 <musabismail02@gmail.com>
2024-05-18 10:29:17 +0500

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -44,6 +44,34 @@ void String:: display() const
 		cout << data;
 }
 
+char& String::at(int index)
+{
+	if (data == nullptr)
+		exit(0);
+	if (index >= 0 && index < getLength())
+		return data[index];
+	else 
+		return data[0];
+}
+
+const char& String::at(const int index)const
+{
+	if (data == nullptr)
+		exit(0);
+	else if (index >= 0 && index < getLength())
+		return data[index];
+	else
+		return data[0];
+}
+
+String::String(const String & ref)
+{
+	if (ref.isEmpty())
+		return;
+	size = ref.size;
+	data = new char[size];
+	copyString(data, ref.data);
+}
 String::~String()
 {
 	if(data!=nullptr)
